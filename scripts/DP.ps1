f00b3a1a44674f73a529ee80c1489bff60ecb4bc
Xzonn <Xzonn@outlook.com>
2023-10-09 19:08:12 +0800

    HGSS: Update files

--- scripts/DP.ps1
+++ scripts/DP.ps1
@@ -2,7 +2,9 @@ $PCTRTools = "tools/PCTRTools/bin/Release/PCTRTools.exe"
 
 # Diamond
 New-Item -ItemType "Directory" -Path "out/D/data/" -Force
+New-Item -ItemType "Directory" -Path "out/D/overlay/" -Force
 Copy-Item -Path "files/D/*.bin" -Destination "out/D/" -Recurse -Force
+Copy-Item -Path "files/D/overlay/*" -Destination "out/D/overlay/" -Recurse -Force
 
 dotnet script scripts/D.csx
 & "$PCTRTools" "replace-narc" -i "files/DP/data/" -n "textures/DP/" -o "out/D/data/"
@@ -12,7 +14,9 @@ Compress-Archive -Path "out/D/*" -DestinationPath "out/Patch-D.zip" -Force
 
 # Pearl
 New-Item -ItemType "Directory" -Path "out/P/data/" -Force
+New-Item -ItemType "Directory" -Path "out/P/overlay/" -Force
 Copy-Item -Path "files/P/*.bin" -Destination "out/P/" -Recurse -Force
+Copy-Item -Path "files/P/overlay/*" -Destination "out/P/overlay/" -Recurse -Force
 
 dotnet script scripts/P.csx
 Copy-Item -Path "out/D/data/*" -Destination "out/P/data/" -Recurse -Force
--- scripts/HGSS.ps1
+++ scripts/HGSS.ps1
@@ -1,18 +1,25 @@
 $PCTRTools = "tools/PCTRTools/bin/Release/PCTRTools.exe"
 
 # Heart Gold
-New-Item -ItemType "Directory" -Path "out/HG/" -Force
-Copy-Item -Path "files/HGSS/*" -Destination "out/HG/" -Recurse -Force
-Copy-Item -Path "files/HG/*" -Destination "out/HG/" -Recurse -Force
+New-Item -ItemType "Directory" -Path "out/HG/data/" -Force
+New-Item -ItemType "Directory" -Path "out/HG/overlay/" -Force
+Copy-Item -Path "files/HGSS/overlay/*" -Destination "out/HG/overlay/" -Recurse -Force
+Copy-Item -Path "files/HG/*.bin" -Destination "out/HG/" -Force
+Copy-Item -Path "files/HG/overlay/*" -Destination "out/HG/overlay/" -Recurse -Force
+
+dotnet script scripts/HG.csx
+& "$PCTRTools" "replace-narc" -i "files/HGSS/data/" -n "textures/HGSS/" -o "out/HG/data/"
 & "$PCTRTools" "font" -c "files/CharTable.txt" -i "files/HGSS/data/a/0/1/6" -o "out/HG/data/a/0/1/6"
 & "$PCTRTools" "text-import" -c "files/CharTable.txt" -i "files/HGSS/data/a/0/2/7" -t "files/HGSS/Messages.txt" -o "out/HG/data/a/0/2/7"
-Remove-Item -Path "out/HG/Messages.txt" -Force
 Compress-Archive -Path "out/HG/*" -DestinationPath "out/Patch-HG.zip" -Force
 
 # Soul Silver
-New-Item -ItemType "Directory" -Path "out/SS/" -Force
-Copy-Item -Path "files/HGSS/*" -Destination "out/SS/" -Recurse -Force
-Copy-Item -Path "files/SS/*" -Destination "out/SS/" -Recurse -Force
-Copy-Item -Path "out/HG/data/a/0/1/6" -Destination "out/SS/data/a/0/1/6"
-Copy-Item -Path "out/HG/data/a/0/2/7" -Destination "out/SS/data/a/0/2/7"
+New-Item -ItemType "Directory" -Path "out/SS/data/" -Force
+New-Item -ItemType "Directory" -Path "out/SS/overlay/" -Force
+Copy-Item -Path "files/HGSS/overlay/*" -Destination "out/SS/overlay/" -Recurse -Force
+Copy-Item -Path "files/SS/*.bin" -Destination "out/SS/" -Force
+Copy-Item -Path "files/SS/overlay/*" -Destination "out/SS/overlay/" -Recurse -Force
+
+dotnet script scripts/SS.csx
+Copy-Item -Path "out/HG/data/*" -Destination "out/SS/data/" -Recurse -Force
 Compress-Archive -Path "out/SS/*" -DestinationPath "out/Patch-SS.zip" -Force
