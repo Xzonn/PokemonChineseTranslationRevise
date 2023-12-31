20416bc28fbc42881e166d44dc8b93cca8722787
Xzonn <Xzonn@outlook.com>
2023-12-31 22:34:33 +0800

    Update translation

--- scripts/create_txt.py
+++ scripts/create_txt.py
@@ -3,47 +3,61 @@ import os
 
 os.chdir(os.path.join(os.path.dirname(__file__), "../"))
 
-GAMES = [
+GAMES: list[dict[str, str | list]] = [
   {
     "game": "DP",
-    "file_name": "Messages.txt",
-    "output_folder": "DP",
+    "folder_name": "messages",
+    "file_list": "messages_list.txt",
+    "file_name": "messages.txt",
+    "json_folder": "DP",
     "languages": ["zh_Hans"],
   },
   {
     "game": "Pt",
-    "file_name": "Messages.txt",
-    "output_folder": "Pt",
+    "folder_name": "messages",
+    "file_list": "messages_list.txt",
+    "file_name": "messages.txt",
+    "json_folder": "Pt",
     "languages": ["zh_Hans"],
   },
   {
     "game": "HGSS",
-    "file_name": "Messages.txt",
-    "output_folder": "HGSS",
+    "folder_name": "messages",
+    "file_list": "messages_list.txt",
+    "file_name": "messages.txt",
+    "json_folder": "HGSS",
     "languages": ["zh_Hans"],
   },
   {
     "game": "BW",
-    "file_name": "Messages_common.txt",
-    "output_folder": "BW_common",
+    "folder_name": "messages_common",
+    "file_list": "messages_common_list.txt",
+    "file_name": "messages_common.txt",
+    "json_folder": "BW_common",
     "languages": ["zh_Hans", "zh_Hant"],
   },
   {
     "game": "BW",
-    "file_name": "Messages_script.txt",
-    "output_folder": "BW_script",
+    "folder_name": "messages_script",
+    "file_list": "messages_script_list.txt",
+    "file_name": "messages_script.txt",
+    "json_folder": "BW_script",
     "languages": ["zh_Hans", "zh_Hant"],
   },
   {
     "game": "B2W2",
-    "file_name": "Messages_common.txt",
-    "output_folder": "B2W2_common",
+    "folder_name": "messages_common",
+    "file_list": "messages_common_list.txt",
+    "file_name": "messages_common.txt",
+    "json_folder": "B2W2_common",
     "languages": ["zh_Hans", "zh_Hant"],
   },
   {
     "game": "B2W2",
-    "file_name": "Messages_script.txt",
-    "output_folder": "B2W2_script",
+    "folder_name": "messages_script",
+    "file_list": "messages_script_list.txt",
+    "file_name": "messages_script.txt",
+    "json_folder": "B2W2_script",
     "languages": ["zh_Hans", "zh_Hant"],
   },
 ]
@@ -55,7 +69,7 @@ for game_info in GAMES:
   }
   game = game_info["game"]
   file_name = game_info["file_name"]
-  output_folder = game_info["output_folder"]
+  json_folder = game_info["json_folder"]
   languages = game_info["languages"]
 
   with open(f"master/files/{game}/{file_name}", "r", -1, "utf8") as reader:
@@ -85,26 +99,30 @@ for game_info in GAMES:
   for language in languages:
     if language not in game_data:
       game_data[language] = {}
+    if not os.path.exists(f"{json_folder}/{language}/"):
+      continue
 
-    for json_name in os.listdir(f"{output_folder}/{language}/"):
+    for json_name in os.listdir(f"{json_folder}/{language}/"):
       json_id = int(json_name.removesuffix(".json"))
       if not json_name.endswith(".json"):
         continue
 
-      if (output_folder == "B2W2_common" and 113 <= json_id <= 157) or \
-         (output_folder == "BW_common" and 93 <= json_id <= 137):
-        path = f"{output_folder}/ja/{json_name}"
+      if (json_folder == "B2W2_common" and 113 <= json_id <= 157) or \
+         (json_folder == "BW_common" and 93 <= json_id <= 137):
+        path = f"{json_folder}/ja/{json_name}"
       else:
-        path = f"{output_folder}/{language}/{json_name}"
+        path = f"{json_folder}/{language}/{json_name}"
       with open(path, "r", -1, "utf8") as reader:
         raw_data = json.load(reader)
 
       for key, chinese in raw_data.items():
         k_game, file_id, line_id = key.split(".")
-        if output_folder != k_game:
+        if json_folder != k_game:
           continue
         else:
           file_id = int(file_id)
+        if file_id not in original_style:
+          continue
         if file_id not in game_data[language]:
           game_data[language][file_id] = {}
         
