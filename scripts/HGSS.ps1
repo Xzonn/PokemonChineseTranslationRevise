239d164850d81b62292b7c928792497a89d0943c
Xzonn <Xzonn@outlook.com>
2024-10-11 12:27:56 +0800

    Fix tool path

--- scripts/HGSS.ps1
+++ scripts/HGSS.ps1
@@ -1,4 +1,4 @@
-$PCTRTools = "tools/PCTRTools/bin/Release/PCTRTools.exe"
+$PCTRTools = "tools/PCTRTools/bin/Release/net8.0/publish/PCTRTools.exe"
 
 # Replace narc
 & "$PCTRTools" "replace-narc" -i "original_files/HGSS/data/" -n "textures/HGSS/" -o "out/HG/data/"
